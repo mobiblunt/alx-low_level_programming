10442c103b5d1ba617c315d21f8eaf399b1c7cf1
blunt <mobiblunt@gmail.com>
2023-02-17 14:45:52 -0800

    ifelse0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints a message to the console
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d ", n);
+if (n > 0)
+printf("is positive\n");
+else if (n < 0)
+printf("is negative\n");
+else
+printf("is zero\n");
+return (0);
+}
