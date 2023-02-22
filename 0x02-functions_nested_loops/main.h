a4091542f0149641cd8df87db0d7cdef13ea4227
blunt <mobiblunt@gmail.com>
2023-02-21 19:26:28 -0800

    check

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to print
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
