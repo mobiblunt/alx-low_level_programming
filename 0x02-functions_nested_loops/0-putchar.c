f3f593057f602f912047b826b1f5be9c6d9dedb3
blunt <mobiblunt@gmail.com>
2023-02-21 19:08:17 -0800

    firtst

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints a message to the console
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
