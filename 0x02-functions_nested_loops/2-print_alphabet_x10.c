a48f9e5de57264226bda5dd33870ad7d3303207d
blunt <mobiblunt@gmail.com>
2023-02-21 20:10:16 -0800

    dew

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * This program prints a message to the console
- *
- * Return: Always 0 (Success)
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
  */
-
 void print_alphabet_x10(void)
 {
+char c;
 int i;
 
 for (i = 0; i < 10; i++)
 {
-print_alphabet();
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
 }
