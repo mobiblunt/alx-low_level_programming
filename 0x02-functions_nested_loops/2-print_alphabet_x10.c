99e0a1fb224ecf99e81443dddfe2b372613374e4
blunt <mobiblunt@gmail.com>
2023-02-21 20:02:33 -0800

    nursewhine

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * This program prints a message to the console
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+print_alphabet();
+}
+}
