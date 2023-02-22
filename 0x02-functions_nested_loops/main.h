c02d375c9534aa07ef4510bc4d09f8bf0a2131d5
blunt <mobiblunt@gmail.com>
2023-02-21 19:45:36 -0800

    lasybust

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,4 +11,26 @@
 
 int _putchar(char c);
 
+
+/**
+ * print_alphabet - writes a character to stdout
+ * @c: the character to print
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - writes a character to stdout
+ * @c: the character to print
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+void print_alphabet_x10(void)
+
 #endif /* MAIN_H */
