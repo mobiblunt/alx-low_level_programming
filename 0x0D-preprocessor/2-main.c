4a8abdeab6b9011e1825e18bcc63d1d86e0babc9
blunt <mobiblunt@gmail.com>
2023-03-19 23:30:46 -0700

    finished macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - concatenates all the arguments of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - concatenates all the arguments of the program
+ *
+ * Return: 0
+ */
+int main(void);
+
+#endif /* MAIN_H */
